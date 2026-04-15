deebe2b0298c3193c22f1d73e20dee303f60be4e
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-15 19:48:50 -0300

    Update 1071.C

--- 1071.C
+++ 1071.C
@@ -1,1 +1,18 @@
+/* --------------------------------------------------------------------------
+Nome        : Otávio Fernandes Caberlin - 195834
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1071
+Exercício   : 1071
+Data        : 14/04/2026
+Objetivo    : Soma de ímpares entre 2 valores.
+Aprendizado : .
+-------------------------------------------------------------------------- */
 
+#include <stdio.h>
+
+int main() {
+
+
+  return 0
+}
