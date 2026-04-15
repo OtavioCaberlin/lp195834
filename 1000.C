5d1d818e41e71b264759a4ceae98ada5f55ba4e3
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-15 19:27:35 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -1,8 +1,19 @@
-//Otávio Fernandes Caberlin - 195834
+/* --------------------------------------------------------------------------
+Nome        : Otávio Fernandes Caberlin - 195834
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1000
+Exercício   : 1000
+Data        : 07/04/2026
+Objetivo    : Imprimir "Hello World!".
+Aprendizado : Estrutura inicial de C e como imprimir algo.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
 
+  // Processamento da saída
   printf("Hello World!\n");
 
   return 0;
