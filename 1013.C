68aecb62357d9ea89844f7486abd479323bd570f
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-14 22:35:53 -0300

    Update 1013.C

--- 1013.C
+++ 1013.C
@@ -1,21 +1,20 @@
 //Otávio Fernandes Caberlin - 195834
 #include <stdio.h>
 
-int main()
-{
-int a, b, c;
+int main() {
+    int a, b, c;
 
-scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c);
 
-if (a >= b && a >= c) {
-    printf("%d eh o maior\n", a);
-}
-else if (b >= a && b >= c) {
-    printf("%d eh o maior\n", b);
-}
-else {
-    printf("%d eh o maior\n", c);
-}
+    if (a >= b && a >= c) {
+        printf("%d eh o maior\n", a);
+    }
+    else if (b >= a && b >= c) {
+        printf("%d eh o maior\n", b);
+    }
+    else {
+        printf("%d eh o maior\n", c);
+    }
 
 return 0;
 }
