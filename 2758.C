17dcf11602956918cf44ca5f01eeb1f1b7e727ac
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-14 22:44:20 -0300

    Update 2758.C

--- 2758.C
+++ 2758.C
@@ -2,35 +2,36 @@
 #include <stdio.h>
 
 int main() {
-  float n1, n2; //precisão simlpes
-  double n3, n4; //precisão dupla
+  // declaração das variáveis 
+  float n1, n2; // precisão simpLes
+  double n3, n4; // precisão dupla
 
-  scanf("%f", &n1); //precisão simples 1 casa decimal 
-  scanf("%f", &n2); //precisão simples 2 casas decimais 
-  scanf("%lf", &n3); //precisão dupla 3 casas decimais 
-  scanf("%lf", &n4); //precisão dupla 4 casas decimais 
+  scanf("%f", &n1); // precisão simples 1 casa decimal 
+  scanf("%f", &n2); // precisão simples 2 casas decimais 
+  scanf("%lf", &n3); // precisão dupla 3 casas decimais 
+  scanf("%lf", &n4); // precisão dupla 4 casas decimais 
 
-  //saída normal
+  // saída normal
   printf("A = %f, B = %f\n", n1, n2);
   printf("C = %lf, D = %lf\n", n3, n4);
 
-  //saída 1 casa decimal
+  // saída 1 casa decimal
   printf("A = %.1f, B = %.1f\n", n1, n2);
   printf("C = %.1lf, D = %.1lf\n", n3, n4);
 
-  //saída 2 casas decimais 
+  // saída 2 casas decimais 
   printf("A = %.2f, B = %.2f\n", n1, n2);
   printf("C = %.2lf, D = %.2lf\n", n3, n4);
 
-  //saída 3 casas decimais 
+  // saída 3 casas decimais 
   printf("A = %.3f, B = %.3f\n", n1, n2);
   printf("C = %.3lf, D = %.3lf\n", n3, n4);
 
-  //saída 3 casas decimais com notação científica
+  // saída 3 casas decimais com notação científica
   printf("A = %.3E, B = %.3E\n", n1, n2);
   printf("C = %.3E, D = %.3E\n", n3, n4);
 
-  //saída número inteiro
+  // saída número inteiro
   printf("A = %.0f, B = %.0f\n", n1, n2);
   printf("C = %.0lf, D = %.0lf\n", n3, n4);
 
