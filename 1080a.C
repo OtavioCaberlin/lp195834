7d1e1e4531792342dd2e76f01ffb51a5750eac2e
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-17 00:07:44 -0300

    Update 1080a.C

--- 1080a.C
+++ 1080a.C
@@ -1,1 +1,18 @@
+/* --------------------------------------------------------------------------
+Nome        : Otávio Fernandes Caberlin - 195834
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1080
+Exercício   : 1080
+Data        : 16/04/2026
+Objetivo    : Maior número entre 100 e sua posição (sem vetores).
+Aprendizado : .
+-------------------------------------------------------------------------- */
 
+#include <stdio.h>
+
+int main () {
+
+  return 0
+
+}
