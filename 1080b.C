597b513389cfa0e27e64fe969463c37d71220d19
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-19 15:06:22 -0300

    Update 1080b.C

--- 1080b.C
+++ 1080b.C
@@ -13,22 +13,29 @@ Aprendizado : Como utilizar vetores e suas aplicações.
 
 int main() {
 
+	// Declaração das variáveis
 	int v[100], maior, entrada, i;
 
+	// Definindo número de loops
 	for(i = 0; i < 100; i++) {
 
+		// Processamento da entrada
 		scanf("%d", &v[i]);
 
+		// Definindo o termo inicial
 		if(i == 0) {
 			maior = v[0];
 			entrada = 1;
 		}
+
+		// Comparando com os demais termos
 		else if(v[i] > maior) {
 			maior = v[i];
 			entrada = i + 1;
 		}
 	}
 
+	// Processamento da saída
 	printf("%d\n", maior);
 	printf("%d\n", entrada);
 
