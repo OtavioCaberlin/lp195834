58294c22c6728b430c1f731a3e6b0df3c2a22dda
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-19 14:38:04 -0300

    Update 1080b.C

--- 1080b.C
+++ 1080b.C
@@ -6,31 +6,31 @@ Plataforma  : https://judge.beecrowd.com/pt/problems/view/1080
 Exercício   : 1080
 Data        : 16/04/2026
 Objetivo    : Maior número entre 100 e sua posição (com vetores).
-Aprendizado : .
+Aprendizado : Como utilizar vetores e suas aplicações.
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 
 int main() {
 
-    int v[100], maior, entrada, i;
+int v[100], maior, entrada, i;
     
-    for(i = 0; i < 100; i++) {
+for(i = 0; i < 100; i++) {
         
-        scanf("%d", &v[i]);
+scanf("%d", &v[i]);
         
-        if(i == 0) {
-            maior = v[0];
-            entrada = 1;
-        }
-        else if(v[i] > maior) {
-            maior = v[i];
-            entrada = i + 1;
-        }
-    }
+if(i == 0) {
+maior = v[0];
+entrada = 1;
+}
+else if(v[i] > maior) {
+maior = v[i];
+entrada = i + 1;
+}
+}
     
-    printf("%d\n", maior);
-    printf("%d\n", entrada);
+printf("%d\n", maior);
+printf("%d\n", entrada);
     
-    return 0;
+return 0;
 }
