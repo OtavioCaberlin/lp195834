e064366593240277b3dea27725546a986951dd6d
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-19 15:19:06 -0300

    Update 2758.C

--- 2758.C
+++ 2758.C
@@ -13,39 +13,39 @@ Aprendizado : Novos tipos de variáveis e formatação e limitação da saída.
 
 int main() {
 
-  // Declaração das variáveis 
-  float n1, n2; // precisão simpLes
-  double n3, n4; // precisão dupla
+	// DeclaraC'C#o das variC!veis
+	float n1, n2; // precisC#o simpLes
+	double n3, n4; // precisC#o dupla
 
-  // Processamento das entradas
-  scanf("%f", &n1); // precisão simples 1 casa decimal 
-  scanf("%f", &n2); // precisão simples 2 casas decimais 
-  scanf("%lf", &n3); // precisão dupla 3 casas decimais 
-  scanf("%lf", &n4); // precisão dupla 4 casas decimais 
+	// Processamento das entradas
+	scanf("%f", &n1); // precisC#o simples 1 casa decimal
+	scanf("%f", &n2); // precisC#o simples 2 casas decimais
+	scanf("%lf", &n3); // precisC#o dupla 3 casas decimais
+	scanf("%lf", &n4); // precisC#o dupla 4 casas decimais
 
-  // Processamento da saída normal
-  printf("A = %f, B = %f\n", n1, n2);
-  printf("C = %lf, D = %lf\n", n3, n4);
+	// Processamento da saC-da normal
+	printf("A = %f, B = %f\n", n1, n2);
+	printf("C = %lf, D = %lf\n", n3, n4);
 
-  // Processamento da saída 1 casa decimal
-  printf("A = %.1f, B = %.1f\n", n1, n2);
-  printf("C = %.1lf, D = %.1lf\n", n3, n4);
+	// Processamento da saC-da 1 casa decimal
+	printf("A = %.1f, B = %.1f\n", n1, n2);
+	printf("C = %.1lf, D = %.1lf\n", n3, n4);
 
-  // Processamento da saída 2 casas decimais 
-  printf("A = %.2f, B = %.2f\n", n1, n2);
-  printf("C = %.2lf, D = %.2lf\n", n3, n4);
+	// Processamento da saC-da 2 casas decimais
+	printf("A = %.2f, B = %.2f\n", n1, n2);
+	printf("C = %.2lf, D = %.2lf\n", n3, n4);
 
-  // Processamento da saída 3 casas decimais 
-  printf("A = %.3f, B = %.3f\n", n1, n2);
-  printf("C = %.3lf, D = %.3lf\n", n3, n4);
+	// Processamento da saC-da 3 casas decimais
+	printf("A = %.3f, B = %.3f\n", n1, n2);
+	printf("C = %.3lf, D = %.3lf\n", n3, n4);
 
-  // Processamento da saída 3 casas decimais com notação científica
-  printf("A = %.3E, B = %.3E\n", n1, n2);
-  printf("C = %.3E, D = %.3E\n", n3, n4);
+	// Processamento da saC-da 3 casas decimais com notaC'C#o cientC-fica
+	printf("A = %.3E, B = %.3E\n", n1, n2);
+	printf("C = %.3E, D = %.3E\n", n3, n4);
 
-  // Processamento da saída número inteiro
-  printf("A = %.0f, B = %.0f\n", n1, n2);
-  printf("C = %.0lf, D = %.0lf\n", n3, n4);
+	// Processamento da saC-da nC:mero inteiro
+	printf("A = %.0f, B = %.0f\n", n1, n2);
+	printf("C = %.0lf, D = %.0lf\n", n3, n4);
 
-  return 0;
+	return 0;
 }
