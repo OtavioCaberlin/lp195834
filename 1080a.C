13eb8b86ba36ce135597f705dd79fc3a8bc5cf21
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-19 14:05:29 -0300

    Update 1080a.C

--- 1080a.C
+++ 1080a.C
@@ -6,12 +6,38 @@ Plataforma  : https://judge.beecrowd.com/pt/problems/view/1080
 Exercício   : 1080
 Data        : 16/04/2026
 Objetivo    : Maior número entre 100 e sua posição (sem vetores).
-Aprendizado : .
+Aprendizado : Comparação de muitos números em um loop muito grande.
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 
 int main() {
+    
+    // Declaração das variáveis
+    int num, maior, entrada, i;
 
-  return 0;
+    // loop de teste dos 100 valores
+    for(i = 1; i < 101; i++) {
+        
+        // Processamento da entrada
+        scanf("%d", &num);
+        
+        // Definindo valores da primeira entrada
+        if(i == 1) {
+            maior = num;
+            entrada = i;
+        }
+        
+        // Comparando com as demais entradas
+        else if(num > maior) {
+            maior = num;
+            entrada = i;
+        }
+    }
+    
+    // Processamento das saídas
+    printf("%d\n", maior);
+    printf("%d\n", entrada);
+    
+    return 0;
 }
