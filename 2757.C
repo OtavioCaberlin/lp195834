b17d996ed9e2b930750b6a4ca2513f7bdc1687cf
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-19 16:02:06 -0300

    Update 2757.C

--- 2757.C
+++ 2757.C
@@ -13,7 +13,7 @@ Aprendizado : Modelos diferentes para apresentar a saída.
 
 int main() {
 
-	// DeclaraC'C#o das variC!veis
+	// Declaração das variáveis
 	int n1,n2,n3;
 
 	// Processamento das entradas
@@ -21,7 +21,7 @@ int main() {
 	scanf("%d", &n2);
 	scanf("%d", &n3);
 
-	// Processamento das saC-das
+	// Processamento das saídas
 	printf("A = %d, B = %d, C = %d\n", n1, n2, n3);
 	printf("A = %10d, B = %10d, C = %10d\n", n1, n2, n3);
 	printf("A = %010d, B = %010d, C = %010d\n", n1, n2, n3);
