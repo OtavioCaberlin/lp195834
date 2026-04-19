f52a494b235f9fc2903d8ec831877e4e5d30af25
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-19 00:35:42 -0300

    Update 1071.C

--- 1071.C
+++ 1071.C
@@ -6,12 +6,44 @@ Plataforma  : https://judge.beecrowd.com/pt/problems/view/1071
 Exercício   : 1071
 Data        : 14/04/2026
 Objetivo    : Soma de ímpares entre 2 valores.
-Aprendizado : .
+Aprendizado : Usar for junto com if, variável temporária e novos operadores.
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 
 int main() {
 
+  // Declaração das variáveis 
+  int A, B, soma = 0;
+
+  // Processamento das entradas
+  scanf("%d", &A);
+  scanf("%d", &B);
+
+  // Caso 1: A = B
+  if(A == B) {
+    printf("%d\n", soma);
+  }
+
+  // Caso 2: A < B
+  else if(A < B) {
+    for(int i = A + 1; i < B; ++i) {
+      if(i % 2 != 0) {
+        soma = soma + i;
+      } 
+    }
+    printf("%d\n", soma);
+  }
+
+  // Caso 3: A > B
+  else {
+    for(int i = B + 1; i < A; ++i) {
+      if(i % 2 != 0) {
+        soma = soma + i;
+      }
+    }
+    printf("%d\n", soma);
+  }
+    
   return 0;
 }
