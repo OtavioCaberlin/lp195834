113c176dab3994951168e2254a34f96261eb3ed9
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-23 20:55:39 -0300

    Create 1234.C

---
+++ 1234.C
@@ -0,0 +1,9 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Otávio Fernandes Caberlin - 195834
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1234
+Data        : 23/04/2026
+Objetivo    : .
+Aprendizado : .
+-------------------------------------------------------------------------- */
