76457877b3e57d0b3ee39b120d37be9f2daaf735
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-23 20:51:36 -0300

    Update 1080a.C

--- 1080a.C
+++ 1080a.C
@@ -1,9 +1,8 @@
 /* --------------------------------------------------------------------------
-Nome        : Otávio Fernandes Caberlin - 195834
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Otávio Fernandes Caberlin - 195834
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/problems/view/1080
-Exercício   : 1080
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
 Data        : 16/04/2026
 Objetivo    : Maior número entre 100 e sua posição (sem vetores).
 Aprendizado : Comparação de muitos números em um loop muito grande.
