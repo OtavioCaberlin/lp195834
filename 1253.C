cbd5475e4b83925cc41baf2d931406c160e0e0d2
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-04-28 00:05:20 -0300

    Update 1253.C

--- 1253.C
+++ 1253.C
@@ -4,6 +4,42 @@ Nome        : Otávio Fernandes Caberlin - 195834
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1253
 Data        : 23/04/2026
-Objetivo    : .
-Aprendizado : .
+Objetivo    : Decodificação da "Cifra de César".
+Aprendizado : Manipulação de caracteres deslocados no alfabeto.
 -------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int main() {
+    
+    // Declaração das variáveis
+    int N, i, j, deslocamento;
+    char string[51];
+
+    // Número de casos
+    scanf("%d", &N); 
+
+    // Repetição de casos
+    for (i = 0; i < N; i++) {
+        
+        // Processamento das entradas 
+        scanf("%s", string); 
+        scanf("%d", &deslocamento); 
+        
+        // Percorre a string
+        for (j = 0; string[j] != '\0'; j++) {
+            
+            // Voltando a letra
+            string[j] = string[j] - deslocamento;
+            
+            // Caso seja menor que A
+            if (string[j] < 'A') { 
+                string[j] = string[j] + 26; 
+            }
+        }
+        
+        // Processamento da saída
+        printf("%s\n", string); 
+    }
+    return 0;
+}
