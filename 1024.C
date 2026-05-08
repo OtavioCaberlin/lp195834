1fd7c8e1ce8dc84c2eb4ce65a2345959aa045afd
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-05-08 00:16:16 -0300

    Update 1024.C

--- 1024.C
+++ 1024.C
@@ -7,3 +7,59 @@ Data        : 07/05/2026
 Objetivo    : .
 Aprendizado : .
 -------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    // Declaração das variáveis
+    int N, tamanho, metade;
+    char texto[1001], invertido[1001];
+
+    // Processamento de entrada
+    scanf("%d", &N);
+    getchar(); 
+
+    for(int i = 0; i < N; i++) {
+
+        fgets(texto, 1001, stdin);
+
+        tamanho = strlen(texto);
+
+        // Remove o '\n'
+        if(texto[tamanho - 1] == '\n') {
+            texto[tamanho - 1] = '\0';
+            tamanho--;
+        }
+
+        // Desloca 3 posições para a direita
+        for(int j = 0; j < tamanho; j++) {
+
+            if((texto[j] >= 'A' && texto[j] <= 'Z') ||
+               (texto[j] >= 'a' && texto[j] <= 'z')) {
+
+                texto[j] = texto[j] + 3;
+            }
+        }
+
+        // Inverte a string
+        for(int j = 0; j < tamanho; j++) {
+            invertido[j] = texto[tamanho - 1 - j];
+        }
+
+        // Fim da string
+        invertido[tamanho] = '\0'; 
+
+        // Desloca 1 para a esquerda da metade para frente
+        metade = tamanho / 2;
+
+        for(int j = metade; j < tamanho; j++) {
+            invertido[j] = invertido[j] - 1;
+        }
+
+        printf("%s\n", invertido);
+    }
+
+    return 0;
+}
