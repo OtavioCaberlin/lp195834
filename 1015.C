88820ebb34079ed4f66dacf4e8c5205f828abb5b
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-05-14 23:09:44 -0300

    Update 1015.C

--- 1015.C
+++ 1015.C
@@ -1,1 +1,9 @@
-
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Otávio Fernandes Caberlin - 195834
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1015
+Data        : 14/05/2026
+Objetivo    : .
+Aprendizado : .
+-------------------------------------------------------------------------- */
