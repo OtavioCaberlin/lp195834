49be0edbd11c22c565e7b75433bdd307ae27e7ca
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-05-26 00:25:03 -0300

    Update 1534a.C

--- 1534a.C
+++ 1534a.C
@@ -1,19 +1,30 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Otávio Fernandes Caberlin - 195834
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1534
+Data        : 12/05/2026
+Objetivo    : Escrever uma matriz.
+Aprendizado : Como fazer um loop para matriz e determinar suas diagonais, usando [].
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
 
+    // Declaração da variável
     int n;
     
-    // Enquanto conseguir ler um número
+    // Loop até chegar no EOF
     while (scanf("%d", &n) != EOF) {
 
         // Criação da matriz
         int matriz[70][70];
 
-        // Percorre linhas
+        // Número de linhas
         for (int i = 0; i < n; i++) {
 
-            // Percorre colunas
+            // Número de colunas
             for (int j = 0; j < n; j++) {
 
                 // Diagonal principal
@@ -21,28 +32,29 @@ int main() {
                     matriz[i][j] = 1;
                 }
 
-                // Diagonal secundária
+                // Diagonal inversa
                 else if (i + j == n - 1) {
                     matriz[i][j] = 2;
                 }
 
-                // Restante da matriz
+                // Resto da matriz
                 else {
                     matriz[i][j] = 3;
                 }
             }
         }
 
-        // Impressão da matriz
+        // Impressão das linhas
         for (int i = 0; i < n; i++) {
 
+            // Impressão das colunas
             for (int j = 0; j < n; j++) {
                 printf("%d", matriz[i][j]);
             }
 
+            // Quebra de linha
             printf("\n");
         }
     }
-
     return 0;
 }
