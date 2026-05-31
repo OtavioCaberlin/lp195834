3c3db5af4e78af8d9bab090ae37bb118f95e654b
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-05-31 12:49:22 -0300

    Create 1029.C

---
+++ 1029.C
@@ -0,0 +1,9 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Otávio Fernandes Caberlin - 195834
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1029
+Data        : 26/05/2026
+Objetivo    : .
+Aprendizado : .
+-------------------------------------------------------------------------- */
