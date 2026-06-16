38f1ac14e4436abf9e752e7d3d3dde3372ce2246
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-06-16 16:59:54 -0300

    Update 1030.C

--- 1030.C
+++ 1030.C
@@ -4,6 +4,44 @@ Nome        : Otávio Fernandes Caberlin - 195834
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1030
 Data        : 09/06/2026
-Objetivo    : .
-Aprendizado : .
+Objetivo    : Caucular a pessoa sobrevivente em uma roda de eliminações.
+Aprendizado : Uso de recursividade em um cilco até sobrar 1.
 -------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int josephus(int x, int y) {
+
+    // Caso 0
+    int sobrevivente = 0; 
+
+    // Calcula proximos indivíduos
+    for(int i = 2; i <= x; i++) {
+        sobrevivente = (sobrevivente + y) % i;
+    }
+
+    // Iniciando em 1
+    return sobrevivente + 1;
+}
+
+int main() {
+
+    // Declaração da variável
+    int casos;
+
+    // Processamento dos casos
+    scanf("%d", &casos);
+
+    for(int i = 1; i <= casos; i++) {
+        
+        int pessoas, salto;
+
+        // Processamento quantidade e tamanho do salto
+        scanf("%d %d", &pessoas, &salto);
+
+        // Processamento da saída 
+        printf("Case %d: %d\n", i, josephus(pessoas, salto));
+    }
+
+    return 0;
+}
