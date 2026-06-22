5952a501441f96fe3a76c1daee037fafc1196a57
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-06-21 21:04:20 -0300

    Create 1175.C

---
+++ 1175.C
@@ -0,0 +1,29 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Otávio Fernandes Caberlin - 195834
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1175
+Data        : 11/06/2026
+Objetivo    : Inverter um vetor.
+Aprendizado : Fazer a leitura e criar um vetor invertido.
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int main() {
+    
+    int v1[20], v2[20];
+    
+    for(int i = 0; i < 20; i++) {
+        
+        scanf("%d", &v1[i]);
+        v2[19 - i] = v1[i];
+    }
+    
+    for(int i = 0; i < 20; i++) {
+        
+        printf("N[%d] = %d\n", i, v2[i]); 
+    }
+
+    return 0;
+}
