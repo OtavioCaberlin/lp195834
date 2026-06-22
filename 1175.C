3cb4ee3f281bc2c996bc1bee0fb334d60e7e8f2e
OtavioCaberlin <otavio.caberlin@unifesp.br>
2026-06-21 22:13:28 -0300

    Update 1175.C

--- 1175.C
+++ 1175.C
@@ -11,17 +11,22 @@ Aprendizado : Fazer a leitura e criar um vetor invertido.
 #include <stdio.h>
 
 int main() {
-    
+
+    // Declaração das variáveis
     int v1[20], v2[20];
-    
+
+    // Loop para ler o vetor
     for(int i = 0; i < 20; i++) {
-        
+
+        // Leitura e inversão do vetor
         scanf("%d", &v1[i]);
         v2[19 - i] = v1[i];
     }
-    
+
+    // Loop de impressão do vetor invertido
     for(int i = 0; i < 20; i++) {
-        
+
+        // Processamento de saída
         printf("N[%d] = %d\n", i, v2[i]); 
     }
 
